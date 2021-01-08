2709f61db0197c9567de926c1f6d246e1f5a6a76
Michelle Li <limich@mit.edu>
2021-01-07 18:58:55 -0800

    grid methods

--- JuliaWorld/src/JuliaWorld.jl
+++ JuliaWorld/src/JuliaWorld.jl
@@ -37,4 +37,34 @@ function generateGrid()
     return Grid(agents, spaces)
 end
 
+function evolveGridOneStep(grid::Grid)
+    dims = size(grid.spaces)
+    r = rand(1:dims[1])
+    c = rand(1:dims[2])
+    grid.spaces[r,c] = rand([grass, rock, water, dirt])
+end
+
+function evolveGridFromAction(agent::Agent, grid::Grid, action::Action)
+    # assert agent in grid.agents keys
+    @assert agent in keys(grid.agents)
+    # compute new loc
+    action_map = Dict{Action,Pair{Integer,Integer}}(up=>Pair(-1,0),down=>Pair(1,0),left=>Pair(0,-1),right=>Pair(0,1),stay=>Pair(0,0))
+    old_loc = grid.agents[agent]
+    drdc = action_map[action]
+    new_loc = Pair(old_loc[1]+drdc[1], old_loc[2]+drdc[2])
+    # clamp
+    dims = size(grid.spaces)
+    clamped_r = clamp(new_loc[1], 1, dims[1])
+    clamped_c = clamp(new_loc[2], 1, dims[2])
+    # update agent loc in grid
+    grid.agents[agent] = Pair(clamped_r, clamped_c)
+end
+
+function main()
+    grid = generateGrid()
+    agent = Agent()
+    grid.agents[agent] = Pair(1,1)
+    evolveGridFromAction(agent, grid, right)
+end
+
 end # module
