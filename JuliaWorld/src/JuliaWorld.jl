3a57ec3b2080826452f2274a8e58b2df220fbe8b
Amber Li <amli@mit.edu>
2021-01-07 20:57:16 -0500

    starting code

---
+++ JuliaWorld/src/JuliaWorld.jl
@@ -0,0 +1,35 @@
+module JuliaWorld
+
+struct Space end
+
+struct Grass <: Space end
+struct Rock <: Space end
+struct Water <: Space end
+struct Dirt <: Space end
+
+struct Agent end
+
+mutable struct Grid
+    agents::Dict{Agent,Pair{Integer,Integer}}
+    spaces::Array{Space,2}
+end
+
+@enum Action begin
+    up = 1
+    down = 2
+    left = 3
+    right = 4
+    stay = 5
+end
+
+function generateGrid()
+    agents = Dict()
+    rows = rand(3:5)
+    cols = rand(3:5)
+    spaces = rand([Grass, Rock, Water, Dirt], (rows, cols))
+    return Grid(agents, spaces)
+end
+
+
+
+end # module
